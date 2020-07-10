7b5225cb15a78d2ede3c181786e8d58cd18615a3
cowboy <tvleanjohnny@gmail.com>
2020-07-10 15:46:44 +0300

    Dev content

---
+++ src/Components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {View, Button, ImageBackground, StyleSheet} from 'react-native'
+import { Input } from './Input'
+
+export const Content = props => {
+    return (
+        <View>
+            <ImageBackground 
+                source={require('../Components/assets/bgcontent.png')}
+                style={{width: '100%', height: '100%'}} 
+            >
+                <View>
+                    <Input />
+                    <Button 
+                        title='Добавить'
+                    />
+                </View>
+            </ImageBackground>
+        </View>
+    )
+} 
